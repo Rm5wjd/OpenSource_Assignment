a704d7d9c8c405556237e600954294a7aa55ad37
Rm5wjd <audrms58365@naver.com>
2024-10-05 14:26:57 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+# 두 정수를 입력받는 프로그램
+
+# 사용자로부터 두 정수를 입력받기
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+# 합과 차 계산
+sum_result = num1 + num2
+difference_result = num1 - num2
+
+# 결과 출력
+print(f"{num1} + {num2} = {sum_result}")
+print(f"{num1} - {num2} = {difference_result}")
